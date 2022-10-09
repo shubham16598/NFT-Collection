30101a1b09dfbc7693c2ebefddb27533b80e27f2
Shubham Singh Kalyanwat <shubham16598@gmail.com>
2022-10-10 00:53:07 +0530

    Smart Contracts done

--- hardhat-tutorial/README.md
+++ hardhat-tutorial/README.md
@@ -11,3 +11,4 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
+// Deployed Address 0x635de9A722BA45E618b7ABE02eb0f64be8F842E7
---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xDaD47a0aD630E3032839b48b69b022a0a3F9BD81";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
